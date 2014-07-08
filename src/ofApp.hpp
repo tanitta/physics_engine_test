23ab9b345b114e70b4eeb949d344534094a7e355
tanitta <ttata.trit@gmail.com>
2014-07-09 04:50:48 +0900

    Add ublas::matrix

--- src/ofApp.hpp
+++ src/ofApp.hpp
@@ -1,11 +1,19 @@
 #pragma once
 
 #include "ofMain.h"
+#include <boost/numeric/ublas/matrix.hpp>         // (1) 普通の行列用のヘッダ
+#include <boost/numeric/ublas/triangular.hpp>     // (2) 三角行列用のヘッダ
+#include <boost/numeric/ublas/symmetric.hpp>      // (3) 対称行列用のヘッダ
+#include <boost/numeric/ublas/hermitian.hpp>      // (4) エルミート行列用のヘッダ
+#include <boost/numeric/ublas/matrix_sparse.hpp>  // (5) 疎行列用のヘッダ
+#include <boost/numeric/ublas/vector.hpp>         // (6) ベクトル用のヘッダ
+#include <boost/numeric/ublas/vector_sparse.hpp>  // (7) 疎ベクトル用のヘッダ
+using namespace boost::numeric;         // boost::numeric
 
 class ofApp : public ofBaseApp{
-
 	public:
-		ofApp(){};
+		ublas::matrix<double> mat_d;
+		ofApp():mat_d(3,4){};
 		~ofApp(){};
 		
 		void setup(){};
