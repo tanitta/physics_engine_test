6091e7b0db5c5cc639a9f866b8183b2f749bca36
tanitta <ttata.trit@gmail.com>
2014-09-08 23:48:20 +0900

    Delete class in physics_entity.hpp

--- src/physics_entity.hpp
+++ src/physics_entity.hpp
@@ -9,45 +9,11 @@
 //state
 namespace physics_engine {
 	namespace entity{
-		class PhysicsEntityCollidable {
-			public:
-				PhysicsEntityCollidable(){};
-				virtual ~PhysicsEntityCollidable(){};
-		};
-
-		class PhysicsEntityShape {
-			public:
-				PhysicsEntityShape(){};
-				virtual ~PhysicsEntityShape(){};
-		};
-
-		class PhysicsEntityAttibute {
-			public:
-				PhysicsEntityAttibute(){};
-				virtual ~PhysicsEntityAttibute(){};
-		};
-
 		class PhysicsEntityState {
 			public:
 				PhysicsEntityState(){};
 				virtual ~PhysicsEntityState(){};
 		};
 
-		class BaseEntity {
-			private:
-				boost::numeric::ublas::vector<double> inertia_;
-				float mass_;
-				float restitution_;
-				float friction_;
-
-
-			public:
-				BaseEntity():
-					inertia_(3,1),
-					mass_(1),
-					restitution_(0.5),
-					friction_(0.5){};
-				virtual ~BaseEntity(){};
-		};
 	} // namespace entity
 } // namespace physics_engine
